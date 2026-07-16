15eedead5f66d2817308a0c34af353be7c8e6f5b
PACHA-HEMANTH-SAI <pachahemanthsai@gmail.com>
2026-07-16 19:53:38 +0530

    Time: 1 ms (84.25%) | Memory: 42.7 MB (73.52%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        String arr[] = s.split(" ");
+
+        if(arr.length != pattern.length()) return false;
+
+        for(int i = 0;i < pattern.length();i++) {
+            if(map.containsKey(pattern.charAt(i))) {
+                if(map.get(pattern.charAt(i)).equals(arr[i])) continue;
+                else return false;
+            }else {
+                map.put(pattern.charAt(i), arr[i]);
+            }
+        }
+
+        for(var i : map.entrySet()) {
+            set.add(i.getValue());
+        }
+
+        if(set.size() != map.size()) return false;
+        return true;
+    }
+}
