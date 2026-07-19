7ee4ac755cf6906dc790fe74633e26414738cb25
PACHA-HEMANTH-SAI <pachahemanthsai@gmail.com>
2026-07-20 05:27:01 +0530

    Time: 22 ms (19.44%) | Memory: 98.2 MB (27.65%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -2,21 +2,25 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> l = new ArrayList<>();
-        generateCombinations(n, k, 1, list, l);
+        generateCombinations( n, 1, k, list, l);
+
         return list;
     }
 
-    public void generateCombinations(int n, int k, int ind, List<List<Integer>> list, List<Integer> l) {
-        if(ind == (n+1)) {
-            if(l.size() == k) {
-                list.add(new ArrayList<>(l));
-            }
+    public void generateCombinations(int n, int ind, int k, List<List<Integer>> list, List<Integer> l) {
+        if(l.size() == k) {
+            list.add(new ArrayList<>(l));
             return;
         }
 
-        generateCombinations(n, k, ind + 1, list, l);
+        else if(ind == (n + 1)) {
+            if(l.size() == k) list.add(new ArrayList<>(l));
+            return;
+        }
+
+        if((list.size() + n - ind) >= k) generateCombinations(n, ind + 1, k, list, l);
         l.add(ind);
-        generateCombinations(n, k, ind + 1, list, l);
+        generateCombinations(n, ind + 1, k, list, l);
         l.remove(l.size() - 1);
     }
 }
