e37effa358dc14f1f9c58d73b7834f9897e61154
PACHA-HEMANTH-SAI <pachahemanthsai@gmail.com>
2026-08-04 22:00:22 +0530

    Time: 2 ms (8.65%) | Memory: 44.5 MB (5.05%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int even = 0;
+        int odd = 0;
+        for(int i = 0; i < num.length();i++) {
+            if((i + 1) % 2 == 0) even = even + Integer.parseInt(num.charAt(i) + "");
+            else 
+            odd = odd + Integer.parseInt(num.charAt(i) + "");
+        }
+
+        return even == odd;
+    }
+}
